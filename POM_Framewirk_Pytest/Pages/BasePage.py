985eb4941b3959af08331d990d95aee869d75cb2
PraveenAutomation <praveen.seleniumautomation@gmail.com>
2021-04-28 22:21:36 +0530

    Added do_send_keys & get_element_text Utility

--- POM_Framewirk_Pytest/Pages/BasePage.py
+++ POM_Framewirk_Pytest/Pages/BasePage.py
@@ -12,3 +12,14 @@ class BasePage:
 
     def do_click(self,by_locator):
         WebDriverWait(self.driver,10).until(EC.visibility_of_element_located(by_locator)).click()
+
+    def do_sen_keys(self,by_locator,text):
+        WebDriverWait(self.driver,10).until(EC.visibility_of_element_located(by_locator)).send_keys(text)
+
+    def get_element_text(self,by_locator):
+        element=WebDriverWait(self.driver,10).until(EC.visibility_of_element_located(by_locator))
+        return element.text
+
+    def is_enabled(self,by_locator):
+        element=WebDriverWait(self.driver,10).until(EC.visibility_of_element_located(by_locator))
+        return bool(element)
---
+++ PythonFAQ/ListAndDictComprenhension.py
@@ -0,0 +1,13 @@
+li=[4,5,9,3,8]
+
+li1=[4,5,4,3,9,3,8]
+
+
+square_list=[x**2 for x in li]
+print(square_list)
+
+dict_sq_list={x:x*x for x in li}
+print(dict_sq_list)
+
+dict_sq_list1={x:x**2 for x in li1}
+print(dict_sq_list1)
