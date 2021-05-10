000e5f86c25c495db110045169e7bd307deb5f8d
PraveenDataScience <praveen.mldatascience@gmail.com>
2021-05-10 21:14:20 +0530

    Added Indedo-1

---
+++ roughWork/Incedof_1.py
@@ -0,0 +1,15 @@
+# Reverse of a List:
+l=[1,4,8,9,6,5,2,3]
+r=l[::-1]
+print(r)
+
+# extend() & append() in List:
+
+L1=[1,2,3,4,5]
+L2=[6,7,8]
+#print(L1.append(L2)) # [1,2,3,4,5,[6,7,8]]
+#L1.extend(L2) # [1,2,3,4,5,6,7,8]
+print(L1)
+#print(L2)
+L3=L1+L2
+print(L3)
