7e2037f4f34d68c9be4dc7da122513e9a4fded96
PraveenAutomation <praveen.seleniumautomation@gmail.com>
2021-05-30 11:52:22 +0530

    Test from San

---
+++ roughWork/Test_1.py
@@ -0,0 +1,28 @@
+x=456
+print("%-06d"%x)
+
+print(0x35 | 0x75)
+
+a=int(43.55+2/20)
+print(a)
+'''
+y="hi"
+print("05d"%y)
+'''
+
+
+print(float('10.8'))
+
+#print('3\')
+print(4.0+float(3))
+b=2
+print(b<<4)
+
+z=-122
+print("-%06d"%z)
+
+m=15
+n=12
+print(m & n)
+
+print(float(22//3+3/3))
