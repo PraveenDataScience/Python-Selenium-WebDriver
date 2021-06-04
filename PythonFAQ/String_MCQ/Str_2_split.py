7d252359410c522bd8726574c75a69cdabd11500
PraveenDataScience <praveen.mldatascience@gmail.com>
2021-06-04 21:33:17 +0530

    Added string utils

---
+++ PythonFAQ/String_MCQ/Str_2_split.py
@@ -0,0 +1,12 @@
+s="VDS TECH LABS"
+l=s.split()
+print(l)
+for x in l:
+    print(x)
+
+print("====================")
+d='05-04-2021'
+l1=d.split('-')
+print(l1)
+for x in l1:
+    print(x)
---
+++ PythonFAQ/String_MCQ/Str_3_join.py
@@ -0,0 +1,8 @@
+l=['VDS', 'TECH', 'LABS']
+s=' '.join(l)
+print(s)
+
+print("====================")
+d=['04','05','2019']
+s1=":".join(d)
+print(s1)
