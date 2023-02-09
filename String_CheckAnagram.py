7ae79d4b463456ecd5912c91c3f868f550739f66
PraveenDataScience <49626999+PraveenDataScience@users.noreply.github.com>
2023-02-09 11:05:41 +0530

    Add files via upload
    Added program to check string is anagram or not in python

---
+++ String_CheckAnagram.py
@@ -0,0 +1,8 @@
+s1=input("Enter String1 ")
+s2=input("Enter String2 ")
+#print(sorted(s1))
+#print(sorted(s2))
+if(sorted(s1)==sorted(s2)):
+    print("Strings are anagarm")
+else:
+    print("Strings are not anagram")
